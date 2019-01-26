3bd1f0238ba99491f129d5a48e789200a90db359
Andras <proci85@hotmail.com>
2019-01-26 23:08:55 +0100

    fetch alpha ok

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -4,17 +4,38 @@ export class Home extends Component {
   static displayName = Home.name;
 
   render () {
+
+    fetch('https://warhammerapi.azurewebsites.net/api/values', {
+      method: 'get',
+      mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(function(response) {
+      console.log(response);
+
+      // return response;
+      // return response.json();
+    }).then(function(data) {
+      // ChromeSamples.log('Created Gist:', data.html_url);
+      console.log(data);
+    });
+
+
     return (
       <div>
         <h1>Army<br/>Ultra First Company</h1>
 
         <img src="ultraSpaceMarine.jpg" alt="Marin" className="center"></img>
 
-        <button className="add-button"><span class="fas fa-angry"></span></button>
+        <button className="add-button">
+          <span>
+            <i className="fas fa-plus"></i>
+          </span>
+        </button>
 
-        <span style="font-size: 3em; color: Tomato;">
-          <i class="fas fa-igloo"></i>
-        </span>
+
       </div>
     );
   }
